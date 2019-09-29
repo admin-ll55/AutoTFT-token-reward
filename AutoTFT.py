f8fcbe961c8dea8f5da3fa30167012cdc92f15f6
admin-ll55 <36874774+admin-ll55@users.noreply.github.com>
2019-09-29 12:37:49 +0800

    Add files via upload

--- AutoTFT.py
+++ AutoTFT.py
@@ -3,6 +3,8 @@ import datetime
 import re
 from win32api import GetSystemMetrics
 import pytesseract
+import psutil
+import win32process
 
 def echo_status():
   global status
@@ -10,7 +12,41 @@ def echo_status():
   print(msg)
   open("AutoTFT.log", 'ab').write((msg+"\n").encode())
   Delay(1)
-
+def get_lolc_hwnd():
+  def callback(hwnd, hwnds):
+    if win32gui.GetWindowText(hwnd) == "League of Legends":
+      rect = win32gui.GetWindowRect(hwnd)
+      x = rect[0]
+      y = rect[1]
+      w = rect[2] - x
+      h = rect[3] - y
+      if len(hwnds) == 0 and w == 1280 and h == 720:
+        hwnds.append(hwnd)
+    return True
+  hwnds = []
+  win32gui.EnumWindows(callback, hwnds)
+  return hwnds
+def get_lolgc_hwnd():
+  def callback(hwnd, hwnds):
+    if win32gui.GetWindowText(hwnd) == "League of Legends (TM) Client":
+      rect = win32gui.GetWindowRect(hwnd)
+      x = rect[0]
+      y = rect[1]
+      w = rect[2] - x
+      h = rect[3] - y
+      if len(hwnds) == 0 and w == 1280 and h == 800:
+        hwnds.append(hwnd)
+    return True
+  hwnds = []
+  win32gui.EnumWindows(callback, hwnds)
+  return hwnds
+fetched = False
+while not fetched:
+  try:
+    lolc_hwnd = get_lolc_hwnd()[0]
+    fetched = True
+  except:
+    fetched = False
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract'
 x, y = (GetSystemMetrics(0), GetSystemMetrics(1))
 lolc = "League of Legends"
@@ -22,14 +58,13 @@ count = 1
 limit = 60*15
 unit_cost_rgb = ((39,57,71),(8,92,39),(15,96,117),(140,31,140),(110,96,64))
 # limit = 1
-
 while True:
   try:
     if status == "":
       if WindowExists(lolgc):
         status = "ingame"
       else:
-        SwitchToWindow(lolc)
+        ShowWindowByHWND(lolc_hwnd)
         Delay(1)
         if FindImage("07_client_play_again.png", 0, 0, x, y, 0.8)[0] > 0:
           status = "ff"
@@ -51,7 +86,7 @@ while True:
       ClickOnImage("12_bugsplat_dont_send.png", 0, 0, x, y, 0.8)
       Delay(1)
     if WindowExists(lolc):
-      SwitchToWindow(lolc)
+      ShowWindowByHWND(lolc_hwnd)
       Delay(1)
       if ClickOnImage("00_client_reconnect_button.bmp", 0, 0, x, y, 0.8):
         status = "queueing"
@@ -71,24 +106,25 @@ while True:
         ALT_TAB()
         Delay(1)
         # SaveImage("[debug]client_play_again", 0, 0, x, y)
-      #7 317
-      #22 322 22 22
-      px, py = FindImage("16_placement.png", 349, 256, 392, 665, 0.8)
-      # SaveImage("[debug]16_placement", 349, 256, 392, 665)
-      SaveImage("[debug]placement", 349+px+15, 256+py+6, 349+px+15+22, 256+py+6+22)
+      opx, opy = FindImage("16_placement_bar.png", 0, 0, x, y, 0.8)
+      opx = opx - 6
+      px, py = FindImage("16_placement.png", opx, opy, opx+43, opy+409, 0.8)
+      # SaveImage("[debug]16_placement", opx, opy, opx+43, opy+409)
+      SaveImage("[debug]placement", opx+px+15, opy+py+6, opx+px+15+22, opy+py+6+22)
       place = str(pytesseract.image_to_string(cv2.bitwise_not(cv2.imread("[debug]placement.png")),config='--psm 7 digit'))
       # print(place)
       # input()
       status = "ended w/ "+place+"th place"
       echo_status()
-      MoveToImage("07_client_play_again.png", 0, 0, x, y, 0.8)
-      Delay(1)
+      # MoveToImage("07_client_play_again.png", 0, 0, x, y, 0.8)
+      # Delay(1)
       ClickOnImage("07_client_play_again.png", 0, 0, x, y, 0.8)
+      MoveMouse(0, 0)
       status = "pending"
       count = 0
       echo_status()
     if WindowExists(lolgc) and status == "ingame":
-      SwitchToWindow(lolgc)
+      ShowWindowByHWND(get_lolgc_hwnd()[0])
       Delay(1)
       #745 828
       #894 828
@@ -111,14 +147,15 @@ while True:
         Delay(1)
         print("\r"+str((limit-z)//60).zfill(2)+"m "+str((limit-z)%60).zfill(2)+"s left", end='')
       print("\r",end='')
-      # SwitchToWindow(lolgc)
+      # ShowWindowByHWND(get_lolgc_hwnd()[0])
       # Delay(1)
       #0 672
       #11 675 25 20
-      hpx, hpy = FindImage("15_hp_corner.png", 1493, 0, x, y, 0.8)
-      SaveImage("[debug]hp", 1493+hpx+11, hpy+3, 1493+hpx+11+25, hpy+3+20)
+      ohpx, ohpy = (0, 0)
+      hpx, hpy = FindImage("15_hp_corner.png", ohpx, ohpy, x, y, 0.8)
+      SaveImage("[debug]hp", ohpx+hpx+11, ohpy+hpy+3, ohpx+hpx+11+25, ohpy+hpy+3+20)
       hp = str(pytesseract.image_to_string(cv2.bitwise_not(cv2.imread("[debug]hp.png")),config='--psm 7 digit'))
-      # print(hp)
+      # print(hpx, hpy, hp)
       # input()
       Delay(1)
       ClickOnImage("05_game_setting_button.png", 0, 0, x, y, 0.8)
@@ -132,7 +169,7 @@ while True:
       echo_status()
       Delay(8)
     if WindowExists(lolgc) and status == "queueing":
-      SwitchToWindow(lolgc)
+      ShowWindowByHWND(get_lolgc_hwnd()[0])
       Delay(1)
       if FindImage("09_rito.png", 0, 0, x, y, 0.8)[0] == -1:
         count += 1
@@ -141,26 +178,26 @@ while True:
       count = 0
       echo_status()
     if WindowExists(lolc) and status == "queueing":
-      SwitchToWindow(lolc)
+      ShowWindowByHWND(lolc_hwnd)
       Delay(1)
       # SaveImage("[debug]client_accept_match_button", 0, 0, x, y)
-      if not ClickOnImage("02_client_accept_match_button.bmp", 0, 0, x, y, 0.8):
+      if not ClickOnImage("02_client_accept_match_button.bmp", 0, 0, x, y, 0.95):
         count += 1
         continue
-      else:
-        MoveMouse(0, 0)
-        count = 0
-        status = "accepted"
-        echo_status()
-        status = "queueing"
-        echo_status()
+      MoveMouse(0, 0)
+      count = 0
+      status = "accepted"
+      echo_status()
+      status = "queueing"
+      echo_status()
     if WindowExists(lolc) and status == "pending":
-      SwitchToWindow(lolc)
+      ShowWindowByHWND(lolc_hwnd)
       Delay(1)
       # SaveImage("[debug]client_find_match_button", 0, 0, x, y)
       if not ClickOnImage("01_client_find_match_button.bmp", 0, 0, x, y, 0.8):
         count += 1
         continue
+      MoveMouse(0, 0)
       status = "queueing"
       count = 0
       echo_status()
